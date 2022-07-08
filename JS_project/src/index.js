8b11a708ff56b8bda2e22e5b0ea8fd9a7cbc9b65
Artstiom Ezepchik <artem.ezepchik@gmail.com>
2022-07-08 14:16:46 +0300

    added window event listener

--- JS_project/src/index.js
+++ JS_project/src/index.js
@@ -2,14 +2,13 @@ import './styles/styles.css'
 
 const li = document.createElement('li')
 const dropDownListsContainer = document.querySelector('.dropdown-lists-container')
-const dropdownContent = document.querySelector('.dropdown-content')
 const makeBtn = document.getElementById('make-btn')
 const modelBtn = document.getElementById('model-btn')
 const yearBtn = document.getElementById('year-btn')
 const makeList = document.getElementById('dropdown-make-list')
 const modelList = document.getElementById('dropdown-model-list')
 const yearList = document.getElementById('dropdown-year-list')
-const resultBtn = document.querySelector('.result-btn')
+const resultBtn = document.getElementById('result-btn')
 const carsUrl = 'http://localhost:3000/cars'
 
 const arrayAppendToList = (array, list) => {
@@ -26,68 +25,87 @@ const arrayAppendToList = (array, list) => {
     return array
 }
 
-const resultBtnInnerText = fetch(carsUrl)
-.then((response) => response.json())
-.then(data => {
-        const carsCount = data.length
-        let aMakeArray = []
-        let modelsArray = []
-        let yearsArray = []
+const carsServerResponseJson = fetch(carsUrl).then((response) => response.json())
 
-        for (let item of data) {
-            if (!aMakeArray.includes(item.make)) {
-                aMakeArray.push(item.make)
-            }
-
-            if (!modelsArray.includes(item.model)) {
-                modelsArray.push(item.model)
-            }
-
-            if (!yearsArray.includes(item.year)) {
-                yearsArray.push(item.year)
-            }
+const makeBtnDropdownBars = carsServerResponseJson.then(data => {
+    let aMakeArray = []
+    for (let item of data) {
+        if (!aMakeArray.includes(item.make)) {
+            aMakeArray.push(item.make)
         }
+    }
 
-        const sortedYearsArray = yearsArray.sort((a,b) => a - b)
-
-        arrayAppendToList(aMakeArray, makeList)
-        arrayAppendToList(modelsArray, modelList)
-        arrayAppendToList(sortedYearsArray, yearList)
-
-        for (let item of aMakeArray) {
-            if (makeBtn.firstChild.textContent.trim() !== item) {
-                modelBtn.classList.add('btn-background-grey')
-            }
-        }
-
-        for (let item of modelsArray) {
-            if (modelBtn.firstChild.textContent.trim() !== item) {
-                yearBtn.classList.add('btn-background-grey')
-            }
-        }
-        
-        if (!carsCount) {
-            return resultBtn.innerHTML = '<span>There are no ads, come back later</span>'
-        }
-
-        return resultBtn.innerHTML  = `<span>View <span class='color-aqua'>${carsCount}</span> available ads</span>`
+    arrayAppendToList(aMakeArray, makeList)
 })
 
-const btnTargetClassesToggle = (btn, list, target) => {
+const btnTargetClassShownToggle = (btn, list, target) => {
     if (target === btn) {
         return list.classList.toggle('shown')
+    }
+}
+
+const removeBtnClassShown = (btn, list, target) => {
+    if (target !== btn && target !== list) {
+        return list.classList.remove('shown')
     }
 }
 
 dropDownListsContainer.addEventListener('click', event => {
     const target = event.target
 
-    btnTargetClassesToggle(makeBtn, makeList, target)
-    btnTargetClassesToggle(modelBtn, modelList, target)
-    btnTargetClassesToggle(yearBtn, yearList, target)
+    btnTargetClassShownToggle(makeBtn, makeList, target)
+    btnTargetClassShownToggle(modelBtn, modelList, target)
+    btnTargetClassShownToggle(yearBtn, yearList, target)
 
     if (target.className === 'inner-text') {
         target.closest('.btn').firstChild.textContent = target.textContent
         target.closest('.dropdown-content').classList.toggle('shown')
+
+        if (target.closest('.btn') === makeBtn) {
+            modelBtn.classList.remove('background-grey-disabled')
+        }
+
+        if (target.closest('.btn') === modelBtn) {
+            yearBtn.classList.remove('background-grey-disabled')
+        }
+
+        carsServerResponseJson.then(data => {
+            const carsCount = data.length
+
+            let modelsArray = []
+            let yearsArray = []
+
+            for (let item of data) {
+                if (target.closest('.btn').firstChild.textContent === item.make) {
+                    if (!modelsArray.includes(item.model)) {
+                        modelsArray.push(item.model)
+                    }
+                }
+
+                if (target.closest('.btn').firstChild.textContent === item.model) {
+                    if (!yearsArray.includes(item.year)) {
+                        yearsArray.push(item.year)
+                    }
+                }
+            }
+
+            const sortedYearsArray = yearsArray.sort((a,b) => a - b)
+
+            arrayAppendToList(modelsArray, modelList)
+            arrayAppendToList(sortedYearsArray, yearList)
+
+            if (!carsCount) {
+                return resultBtn.innerHTML = '<span>There are no ads, come back later</span>'
+            }
+        
+            return resultBtn.innerHTML  = `<span>View <span class='color-aqua'>${carsCount}</span> available ads</span>`
+        })
     }
+})
+
+window.addEventListener('click', event => {
+    const target = event.target
+    removeBtnClassShown(makeBtn, makeList, target)
+    removeBtnClassShown(modelBtn, modelList, target)
+    removeBtnClassShown(yearBtn, yearList, target)
 })
